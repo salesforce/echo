fee71bb4cae19719340f2929275093bcce5f95ed
Paymon Teyer <pteyer@salesforce.com>
2016-09-23 08:22:10 -0700

    better bang script

--- bang.sh
+++ bang.sh
@@ -2,20 +2,29 @@
 
 set -e
 
-PATH_OPENRESTY=src/openresty-1.11.2.1/
+PATH_OPENRESTY=src/openresty-1.11.2.1
+PATH_NGINX_CONFIG=conf/nginx.conf
 
-pushd .
-echo "*** running configure $@..."
-cd ${PATH_OPENRESTY}
-./configure $@
+do_compile() {
+	pushd .
+	echo "*** running configure $@..."
+	cd ${PATH_OPENRESTY}
+	./configure --with-cc-opt="-I/usr/local/include -I/usr/local/opt/openssl/include -I/usr/local/Cellar/pcre/8.39/include/ -I/usr/local/Cellar/openssl/1.0.2g/include/" --with-ld-opt="-L/usr/local/lib -L/usr/local/Cellar/pcre/8.39/lib -L/usr/local/Cellar/openssl/1.0.2g/lib"
+	echo "*** running make..."
+	make
+	popd 
+	echo "*** done."
+}
 
-echo "*** running make..."
-make
+do_run() {
+	sudo ${PATH_OPENRESTY}/build/nginx-1.11.2/objs/nginx -c ${PATH_NGINX_CONFIG}
+}
 
-echo "*** running make install..."
-make install
-
-popd 
-
-echo "*** done."
-
+case "$1" in
+	compile)
+		do_compile
+		;;
+	run)
+		do_run
+		;;
+esac
