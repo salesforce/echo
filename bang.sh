dbb0e079fc7a804341704035bfbb3ed78c55f303
Paymon Teyer <pteyer@salesforce.com>
2016-09-23 11:54:38 -0700

    bang

--- bang.sh
+++ bang.sh
@@ -2,16 +2,43 @@
 
 set -e
 
-PATH_OPENRESTY=src/openresty-1.11.2.1
+OPENRESTY_VERSION=1.11.2.1
+OPENRESTY_PACKAGE=openresty-${OPENRESTY_VERSION}
+
+REDIS_VERSION=stable
+REDIS_PACKAGE=redis-${REDIS_VERSION}
+
 PATH_NGINX_CONFIG=conf/nginx.conf
+do_build_openresty() {
+	echo "*** building openresty..."
+	pushd .
+	mkdir -p /tmp/.echo/
+	cd /tmp/.echo/
+	rm -rf ${OPENRESTY_PACKAGE}
+	wget "https://openresty.org/download/${OPENRESTY_PACKAGE}.tar.gz"
+	tar xfzv ${OPENRESTY_PACKAGE}.tar.gz
+	cd ${OPENRESTY_PACKAGE}
+	./configure --with-cc-opt="-I/usr/local/include -I/usr/local/opt/openssl/include -I/usr/local/Cellar/pcre/8.39/include/ -I/usr/local/Cellar/openssl/1.0.2g/include/" --with-ld-opt="-L/usr/local/lib -L/usr/local/Cellar/pcre/8.39/lib -L/usr/local/Cellar/openssl/1.0.2g/lib"
+	make
+	popd
+	echo "*** openresty build done."
+}
+
+do_build_redis() {
+	echo "*** building redis..."
+	pushd .
+	wget "http://download.redis.io/${REDIS_PACKAGE}.tar.gz"
+	tar xvzf ${REDIS_PACKAGE}.tar.gz
+	cd ${REDIS_PACKAGE}
+	make
+	popd
+	echo "*** redis build done."
+}
 
 do_compile() {
 	pushd .
-	echo "*** running configure $@..."
-	cd ${PATH_OPENRESTY}
-	./configure --with-cc-opt="-I/usr/local/include -I/usr/local/opt/openssl/include -I/usr/local/Cellar/pcre/8.39/include/ -I/usr/local/Cellar/openssl/1.0.2g/include/" --with-ld-opt="-L/usr/local/lib -L/usr/local/Cellar/pcre/8.39/lib -L/usr/local/Cellar/openssl/1.0.2g/lib"
-	echo "*** running make..."
-	make
+	do_build_openresty
+	do_build_redis
 	popd 
 	echo "*** done."
 }
