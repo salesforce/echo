42e82a6fa89c9eb10c0dc27017675d011af38109
Paymon Teyer <pteyer@salesforce.com>
2016-09-23 12:39:51 -0700

    better bang

--- bang.sh
+++ bang.sh
@@ -13,51 +13,64 @@ PATH_TMP=/tmp/.echo
 PATH_NGINX_CONFIG=conf/nginx.conf
 
 do_build_openresty() {
-	echo "*** building openresty..."
-	pushd .
-	rm -rf ${PATH_TMP}
-	mkdir -p ${PATH_TMP}
-	cd ${PATH_TMP}
-	rm -rf ${OPENRESTY_PACKAGE}
-	wget "https://openresty.org/download/${OPENRESTY_PACKAGE}.tar.gz"
-	tar xfzv ${OPENRESTY_PACKAGE}.tar.gz
-	cd ${OPENRESTY_PACKAGE}
-	./configure --prefix=${PATH_TMP} --with-cc-opt="-I/usr/local/include -I/usr/local/opt/openssl/include -I/usr/local/Cellar/pcre/8.39/include/ -I/usr/local/Cellar/openssl/1.0.2g/include/" --with-ld-opt="-L/usr/local/lib -L/usr/local/Cellar/pcre/8.39/lib -L/usr/local/Cellar/openssl/1.0.2g/lib"
-	make
-	popd
-	echo "*** openresty build done."
+  echo "*** building openresty..."
+  pushd .
+  rm -rf ${PATH_TMP}
+  mkdir -p ${PATH_TMP}
+  cd ${PATH_TMP}
+  rm -rf ${OPENRESTY_PACKAGE}
+  wget "https://openresty.org/download/${OPENRESTY_PACKAGE}.tar.gz"
+  tar xfzv ${OPENRESTY_PACKAGE}.tar.gz
+  cd ${OPENRESTY_PACKAGE}
+  if [[ `uname` == 'Darwin' ]]; then
+    ./configure --prefix=${PATH_TMP} \
+             --with-cc-opt="-I /usr/local/include" \
+             --with-ld-opt="-L /usr/local/lib"
+  elif [[ `uname` == 'linux' ]]; then
+    ./configure --prefix=${PATH_TMP} 
+  fi
+  make
+  popd
+  echo "*** openresty build done."
 }
 
 do_build_redis() {
-	echo "*** building redis..."
-	pushd .
-	mkdir -p ${PATH_TMP}
-	cd ${PATH_TMP}
-	wget "http://download.redis.io/${REDIS_PACKAGE}.tar.gz"
-	tar xvzf ${REDIS_PACKAGE}.tar.gz
-	cd ${REDIS_PACKAGE}
-	make
-	popd
-	echo "*** redis build done."
+  echo "*** building redis..."
+  pushd .
+  mkdir -p ${PATH_TMP}
+  cd ${PATH_TMP}
+  wget "http://download.redis.io/${REDIS_PACKAGE}.tar.gz"
+  tar xvzf ${REDIS_PACKAGE}.tar.gz
+  cd ${REDIS_PACKAGE}
+  make
+  popd
+  echo "*** redis build done."
 }
 
 do_compile() {
-	pushd .
-	do_build_openresty
-	do_build_redis
-	popd 
-	echo "*** done."
+  pushd .
+  do_build_openresty
+  do_build_redis
+  popd 
+  echo "*** done."
+}
+
+do_clean() {
+  rm -rf ${PATH_TMP}
 }
 
 do_run() {
-	sudo ${PATH_OPENRESTY}/build/nginx-1.11.2/objs/nginx -c ${PATH_NGINX_CONFIG}
+  sudo ${PATH_OPENRESTY}/build/nginx-1.11.2/objs/nginx -c ${PATH_NGINX_CONFIG}
 }
 
 case "$1" in
-	compile)
-		do_compile
-		;;
-	run)
-		do_run
-		;;
+  clean)
+    do_clean
+    ;;
+  compile)
+    do_compile
+    ;;
+  run)
+    do_run
+    ;;
 esac
