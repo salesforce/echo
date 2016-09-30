5ff7c88b50f37fa047a1b3009db5b186664c9f81
Paymon Teyer <pteyer@salesforce.com>
2016-09-29 17:45:40 -0700

    nginx config

--- bang.sh
+++ bang.sh
@@ -9,9 +9,10 @@ REDIS_VERSION=stable
 REDIS_PACKAGE=redis-${REDIS_VERSION}
 
 PATH_ECHO=~/echo
-PATH_INSTALL=${PATH_ECHO}/pkg
-PATH_NGINX_BIN=${PATH_ECHO}/bin/openresty
-PATH_REDIS=${PATH_INSTALL}/${REDIS_PACKAGE}
+PATH_OPENRESTY=${PATH_ECHO}/openresty
+PATH_ARCHIVE=${PATH_ECHO}/archive
+PATH_NGINX_BIN=${PATH_OPENRESTY}/bin/openresty
+PATH_REDIS=${PATH_ARCHIVE}/${REDIS_PACKAGE}
 PATH_REDIS_BIN=${PATH_REDIS}/src/redis-server
 
 PATH_CONFIG=${PATH_ECHO}/conf
@@ -21,19 +22,15 @@ PATH_REDIS_CONFIG=${PATH_CONFIG}/redis.conf
 do_build_openresty() {
   echo "*** building openresty..."
   pushd .
-  rm -rf ${PATH_INSTALL}
-  mkdir -p ${PATH_INSTALL}
-  cd ${PATH_INSTALL}
-  rm -rf ${OPENRESTY_PACKAGE}
-  wget "https://openresty.org/download/${OPENRESTY_PACKAGE}.tar.gz"
+  cd ${PATH_ARCHIVE}
   tar xfzv ${OPENRESTY_PACKAGE}.tar.gz
   cd ${OPENRESTY_PACKAGE}
   if [[ `uname` == 'Darwin' ]]; then
-    ./configure --prefix=${PATH_ECHO} \
+    ./configure --prefix=${PATH_OPENRESTY} \
              --with-cc-opt="-I /usr/local/include" \
              --with-ld-opt="-L /usr/local/lib"
   elif [[ `uname` == 'linux' ]]; then
-    ./configure --prefix=${PATH_ECHO} 
+    ./configure --prefix=${PATH_OPENRESTY} 
   fi
   make
   make install
@@ -44,9 +41,7 @@ do_build_openresty() {
 do_build_redis() {
   echo "*** building redis..."
   pushd .
-  mkdir -p ${PATH_INSTALL}
-  cd ${PATH_INSTALL}
-  wget "http://download.redis.io/${REDIS_PACKAGE}.tar.gz"
+  cd ${PATH_ARCHIVE}
   tar xvzf ${REDIS_PACKAGE}.tar.gz
   cd ${REDIS_PACKAGE}
   make
@@ -58,9 +53,15 @@ do_copy_configs() {
   cp -r conf ${PATH_ECHO}
 }
 
+do_copy_archive() {
+  cp -r archive ${PATH_ARCHIVE}
+}
+
 do_compile() {
   echo "*** fetch/build..."
   pushd .
+  do_clean
+  do_copy_archive
   do_build_openresty
   do_build_redis
   popd 
@@ -70,17 +71,16 @@ do_compile() {
 
 do_clean() {
   echo "*** cleaning up..."
-  rm -rfv ${PATH_INSTALL} ${PATH_ECHO_BIN} ${PATH_CONFIG}
+  rm -rfv ${PATH_ARCHIVE} ${PATH_ECHO_BIN} ${PATH_CONFIG}
   echo "*** done."
 }
 
 do_run() {
   ${PATH_REDIS_BIN} ${PATH_REDIS_CONFIG}
-  ${PATH_REDIS_BIN} ${PATH_REDIS_SLAVE_1_CONFIG}
-  ${PATH_REDIS_BIN} ${PATH_REDIS_SLAVE_2_CONFIG}
   ${PATH_NGINX_BIN} -c ${PATH_NGINX_CONFIG}
 }
 
+mkdir -p ${PATH_ECHO}
 case "$1" in
   clean)
     do_clean
@@ -88,7 +88,10 @@ case "$1" in
   compile)
     do_compile
     ;;
-  copy)
+  copy_archive)
+    do_copy_archive
+    ;;
+  copy_config)
     do_copy_configs
     ;;
   run)
