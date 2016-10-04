8c4a2136215ed44fc2874330dff565134bc80c93
Paymon Teyer <pteyer@salesforce.com>
2016-10-03 17:04:12 -0700

    notify on lua

--- conf/cache_invalidate.lua
+++ conf/cache_invalidate.lua
@@ -1,7 +1,7 @@
 local invalidate = function(key) 
 	local patterns = redis.call('zrange', key, '0', '-1');
 	for i,v in pairs(patterns) do
-		for _,k in ipairs(redis.call('keys', v)) do 
+		for _,k in ipairs(redis.call('keys', 'object:' .. v)) do 
 			redis.call('del', k) 
 		end
 	end
