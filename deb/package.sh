5245f5d6ca6649e32eaad681b69544ee35b5570d
Paymon <pteyer@salesforce.com>
2016-10-13 17:23:05 -0700

    wip

--- deb/package.sh
+++ deb/package.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+rm -rf echo_0.1-1/opt/echo/conf
+rm -rf echo_0.1-1/opt/echo/lib
+cp -r ../conf echo_0.1-1/opt/echo/conf
+cp -r ../lua/lib echo_0.1-1/opt/echo/lib/lualib
 dpkg-deb --build echo_*
