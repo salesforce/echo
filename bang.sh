c6ad447c07383be096676386a2d1dcb0de02446a
Paymon <pteyer@salesforce.com>
2016-10-13 19:00:43 -0700

    wip

--- bang.sh
+++ bang.sh
@@ -43,41 +43,8 @@ do_copy_archive() {
   cp -r archive ${PATH_ARCHIVE}
 }
 
-do_clean() {
-  echo "*** cleaning up..."
-  rm -rfv ${PATH_ARCHIVE} ${PATH_ECHO_BIN} ${PATH_CONFIG}
-  echo "*** done."
-}
-
-do_start_nginx() {
-  echo "*** starting nginx..."
-  ${PATH_NGINX_BIN} -c ${PATH_NGINX_CONFIG}
-  echo "*** done."
-}
-
-do_start_redis() {
-  echo "*** starting redis..."
-  ${PATH_REDIS_BIN} ${PATH_REDIS_CONFIG}
-  echo "*** done."
-}
-
-do_stop_nginx() {
-  echo "*** stopping nginx..."
-  pkill nginx
-  echo "*** done."
-}
-
-do_stop_redis() {
-  echo "*** stopping redis..."
-  pkill redis-server
-  echo "*** done."
-}
-
 mkdir -p ${PATH_ECHO}
 case "$1" in
-  clean)
-    do_clean
-    ;;
   compile)
     do_copy_archive
     do_build_openresty
@@ -86,30 +53,8 @@ case "$1" in
   copy_archive)
     do_copy_archive
     ;;
-  restart_nginx)
-    do_stop_nginx
-    sleep 1
-    do_start_nginx
-    ;;
-  start_nginx)
-    do_start_nginx
-    ;;
-  restart_redis)
-    do_stop_redis
-    sleep 1
-    do_start_redis
-    ;;
-  start_redis)
-    do_start_redis
-    ;;
-  stop_redis)
-    do_stop_redis
-    ;;
-  stop_nginx)
-    do_stop_nginx
-    ;;
   *)
-    echo $"Usage: $0 {clean|compile|restart_nginx|restart_redis}"
+    echo $"Usage: $0 {compile}"
     exit 1
 esac
 
