c01e3e9ff5c135db1afc9be0fddc1c31696f48a0
Paymon <pteyer@salesforce.com>
2016-10-16 12:53:33 -0700

    wip

--- deb/package.sh
+++ deb/package.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+rm -rf echo_0.1-1/etc/ echo_0.1-1/usr/ 
+mkdir -p echo_0.1-1/etc/
+cp ../conf/redis.conf echo_0.1-1/etc/redis.conf
+mkdir -p echo_0.1-1/etc/init.d/
+cp ../scripts/echo-proxy echo_0.1-1/etc/init.d/echo-proxy
+mkdir -p echo_0.1-1/usr/local/openresty/nginx/conf/
+cp ../conf/nginx.conf echo_0.1-1/usr/local/openresty/nginx/conf/nginx.conf
+
 dpkg-deb --build echo_*
