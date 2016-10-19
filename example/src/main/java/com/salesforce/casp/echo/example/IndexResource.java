876e5a6e5f318a8199c2c037d5ab39571dd96806
Paymon Teyer <pteyer@salesforce.com>
2016-10-19 10:12:03 -0700

    store

--- example/src/main/java/com/salesforce/casp/echo/example/IndexResource.java
+++ example/src/main/java/com/salesforce/casp/echo/example/IndexResource.java
@@ -4,48 +4,58 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Arrays;
-import java.util.Set;
-import java.util.concurrent.ConcurrentSkipListSet;
-
-//        final String payload = new String(new char[1024*1024]).replace('\0', '.');
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Path("/")
 public class IndexResource {
-    private static final String allTokensObservable = "/sfdc/casp/echo/example/all-tokens";
-    private static final Set<String> tokensSet = new ConcurrentSkipListSet<String>();
 
-    private static volatile int token = 0;
+    private static final String allObjectsObservable = "/sfdc/casp/echo/example/allobjects";
+    private static final String objectObservablePrefix = "/sfdc/casp/echo/example/object-id-";
+    private static final Map<String, String> store = new ConcurrentHashMap<String, String>();
+    private static volatile String lastId = "";
 
-    @GET @Path("/last-token") @Produces(MediaType.TEXT_PLAIN)
-    public Response getLastToken() {
-        final String payload = String.valueOf(token);
-        return Response.ok(payload)
-                .header("Content-Length", payload.length())
-                .header("Cache-Control", "max-age=86400")
-                // observe "all-tokens" for when a new token is generated
-                .header("Echo-Observe", allTokensObservable)
+    @POST @Produces(MediaType.TEXT_PLAIN)
+    public Response post(final String body) {
+        final String id = UUID.randomUUID().toString();
+        store.put(id, body);
+        lastId = id;
+        return Response.ok(id)
+                .header("Echo-Notify", allObjectsObservable)
                 .build();
     }
 
-    @POST @Path("/new-token") @Produces(MediaType.TEXT_PLAIN)
-    public Response getCounter() {
-        final String payload = String.valueOf(++token);
-        tokensSet.add(payload);
-        return Response.ok(payload)
-                .header("Content-Length", payload.length())
-                // new token is generated, so notify the all-tokens observable
-                .header("Echo-Notify", allTokensObservable)
+    @POST @Path("/{id}") @Produces(MediaType.TEXT_PLAIN)
+    public Response put(@PathParam("id") final String id, final String body) {
+        store.put(id, body);
+        lastId = id;
+        return Response.ok(id)
+                .header("Echo-Notify", objectObservablePrefix + id)
                 .build();
     }
 
-    @GET @Path("/all-tokens") @Produces(MediaType.TEXT_PLAIN)
+    @GET @Path("/{id}") @Produces(MediaType.TEXT_PLAIN)
+    public Response getObjectById(@PathParam("id") final String id) {
+        final String payload = store.getOrDefault(id, "");
+        return Response.ok(payload)
+                .header("Echo-Observe", objectObservablePrefix + id)
+                .build();
+    }
+
+    @GET @Path("/lastid") @Produces(MediaType.TEXT_PLAIN)
+    public Response getLastId() {
+        return Response.ok(lastId)
+                .header("Echo-Observe", allObjectsObservable)
+                .build();
+    }
+
+    @GET @Path("/all") @Produces(MediaType.TEXT_PLAIN)
     public Response get() {
-        final String payload = Arrays.toString(tokensSet.toArray());
+        final String payload = Arrays.toString(store.entrySet().toArray());
         return Response.ok(payload)
-                .header("Content-Length", payload.length())
-                .header("Cache-Control", "max-age=86400")
                 // observe all-tokens for when a new token is generated
-                .header("Echo-Observe", allTokensObservable)
+                .header("Echo-Observe", allObjectsObservable)
                 .build();
     }
 }
